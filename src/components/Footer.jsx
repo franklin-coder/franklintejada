a6b94ca4014ec2472ea2ff2905d4001fe67b6322
franklin-coder <64282628+franklin-coder@users.noreply.github.com>
2024-01-24 14:22:23 -0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,9 @@ const Footer = () => {
                 <a href="https://ca.linkedin.com/in/franklin-d-tejada-24a033117/en">
                     <img src={linkedin} alt="GitHub" style={{ height: '50px', paddingBottom:'10px', paddingRight:'5px'}} />
                 </a>
+                <a href="https://ca.linkedin.com/in/franklin-d-tejada-24a033117/en">
+                    <img src={linkedin} alt="GitHub" style={{ height: '50px', paddingBottom:'10px', paddingRight:'5px'}} />
+                </a>
                 
             </div>
         </footer>
